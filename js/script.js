aa92ba4ef3bef53746e4ffaf3ed10d6bd14dee42
rviprofile <rvi.profile@gmail.com>
2023-08-05 22:53:08 +0500

    Исправил замечания

--- js/script.js
+++ js/script.js
@@ -1,39 +1,52 @@
+// Задание 1
 let password = 'November1961';
-let userPassword = String(prompt('Введите пароль'))
-
-if (password = userPassword) {
+let userPassword = prompt('Введите пароль')
+if (password === userPassword) {
     alert("Пароль введен верно");
 } else {
     alert("Пароль введен не верно");
 }
 
+// Задание 2
 let c = 3;
-(c >= 0 && c <= 10) ? console.log('Верно') : console.log('Не верно');
+(c > 0 && c < 10) ? console.log('Верно') : console.log('Не верно');
 
+// Задание 3
 let d = 6;
 let e = 55;
-(d >= 100 || e >= 100) ? console.log('Верно') : console.log('Не верно');
+(d > 100 || e > 100) ? console.log('Верно') : console.log('Не верно');
 
+// Задание 4
 let a = '2';
 let b = '3';
 let result = Number(a) + Number(b);
 alert(result);
 
-let monthNumber = 12;
+// Задание 5
+let monthNumber = 11;
 switch (monthNumber) {
-    case 12 || 1 || 2:
+    case 12:
+    case 1:
+    case 2:
         console.log('Зима');
         break;
-    case 3 || 4 || 5:
+    case 3:
+    case 4:
+    case 5:
         console.log('Весна');
         break;
-    case 6 || 7 || 8:
+    case 6:
+    case 7:
+    case 8:
         console.log('Лето');
         break;
-    case 9 || 10 || 11:
+    case 9:
+    case 10:
+    case 11:
         console.log('Осень');
         break;
-    default: console.log('Такого месяца пока нет');
+    default: 
+        console.log('Такого месяца пока нет');
         break;
 }
 
